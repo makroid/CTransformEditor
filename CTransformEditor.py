71d64a60f9f4c3bccf611fbb519bedf5f82362b2
Me <me@example.com>
2020-12-23 14:33:18 +0100

    initial commit

---
+++ CTransformEditor.py
@@ -0,0 +1,395 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QHBoxLayout, QColorDialog, \
+    QPushButton, QLabel, QCheckBox, QComboBox, QSlider, QFormLayout
+from PyQt5.QtGui import QPainter, QColor, QFont, QPixmap, QImage
+from PyQt5.QtCore import Qt
+
+from CTransformGrid import Range, Grid, TFunctions, TMatrix
+
+import numpy as np
+
+
+
+class CellRC:
+    def __init__(self, r, c):
+        self.r = r
+        self.c = c
+
+
+class ActiveCell:
+    def __init__(self, r, c, grid):
+        self.rc = CellRC(r, c)
+        self.grid = grid
+
+    def up(self):
+        self.rc.r -= 1
+        self.rc.r %= self.grid.grid_rows
+        print(self.rc.r)
+        pass
+
+    def down(self):
+        self.rc.r += 1
+        self.rc.r %= self.grid.grid_rows
+        pass
+
+    def left(self):
+        self.rc.c -= 1
+        self.rc.c %= self.grid.grid_cols
+        pass
+
+    def right(self):
+        self.rc.c += 1
+        self.rc.c %= self.grid.grid_cols
+        pass
+
+
+class GridWidget(QWidget):
+    def __init__(self, parent, controlWidget):
+        super().__init__()
+
+        self.parent = parent
+        self.control_widget = controlWidget
+
+        self.grid_rows = 50
+        self.grid_cols = 50
+        self.tile_size_x = 10
+        self.tile_size_y = self.tile_size_x
+
+        self.grid = np.ones((self.grid_rows, self.grid_cols, 3), dtype=np.uint)*255
+
+        self.setMouseTracking(True)
+        self.x_prev = -1
+        self.y_prev = -1
+
+        self.activeCell = None
+
+        self.setMinimumSize(self.grid_cols*self.tile_size_x, self.grid_rows*self.tile_size_y)
+
+
+    def from_image(self, img_path):
+        import cv2
+        img = cv2.imread(img_path)
+        img = cv2.resize(img, (self.grid_rows, self.grid_cols))
+        self.x_prev = -1
+        self.y_prev = -1
+
+        self.activeCell = None
+        self.grid = img[:, :, (2, 1, 0)]
+        self.repaint()
+
+
+    def from_grid(self, grid: Grid):
+        self.x_prev = -1
+        self.y_prev = -1
+        self.activeCell = None
+
+        self.grid_rows = grid.get_range("y").size()
+        self.grid_cols = grid.get_range("x").size()
+
+        self.grid = grid.get_filled().copy()
+        #self.setMinimumSize(self.grid_cols * self.tile_size_x, self.grid_rows * self.tile_size_y)
+        self.repaint()
+
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_grid(event, qp)
+        self.draw_activeCell(qp)
+        qp.end()
+
+
+    def keyPressEvent(self, event):
+        super(GridWidget, self).keyPressEvent(event)
+        if (event.modifiers() & Qt.ControlModifier) and event.key() == Qt.Key_S:
+            print("export")
+            self.export("test_input_tmp.txt")
+
+        if self.activeCell:
+            if event.key() == Qt.Key_Up:
+                self.activeCell.up()
+            if event.key() == Qt.Key_Down:
+                self.activeCell.down()
+            if event.key() == Qt.Key_Left:
+                self.activeCell.left()
+            if event.key() == Qt.Key_Right:
+                self.activeCell.right()
+            self.repaint()
+
+
+    def mousePressEvent(self, event):
+        x = event.x()
+        y = event.y()
+        if event.buttons() == Qt.LeftButton:
+            self.activeCell = None
+            self.fillCell(x, y)
+            self.repaint()
+        if event.buttons() == Qt.RightButton:
+            co = self.get_cell_coords(x, y)
+            if co:
+                self.activeCell = ActiveCell(co.r, co.c, self)
+                self.repaint()
+
+
+    def mouseReleaseEvent(self, event):
+        self.x_prev = -1
+        self.y_prev = -1
+
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() == Qt.LeftButton:
+            x = event.x()
+            y = event.y()
+            if not (int(x / self.tile_size_x) - int(self.x_prev / self.tile_size_x) == 0 and \
+                int(y / self.tile_size_y) - int(self.y_prev / self.tile_size_y) == 0):
+                self.fillCell(x, y)
+                self.repaint()
+            self.x_prev = x
+            self.y_prev = y
+
+
+    def get_cell_coords(self, x, y):
+        pc = int(x / self.tile_size_x)
+        pr = int(y / self.tile_size_y)
+        if pr >= 0 and pr < self.grid_rows and pc >= 0 and pc < self.grid_cols:
+            return CellRC(pr, pc)
+        else:
+            return None
+
+
+    def fillCell(self, x, y):
+        co = self.get_cell_coords(x,y)
+        if co:
+            self.grid[co.r][co.c] = self.control_widget.color_button.color_rgb()
+            self.parent.update_output_canvas()
+
+
+    def draw_activeCell(self, qp):
+        if self.activeCell:
+            r = self.activeCell.rc.r
+            c = self.activeCell.rc.c
+            self.grid[r][c] = 255 - self.grid[r][c]
+            qp.setBrush(QColor(self.grid[r][c][0], self.grid[r][c][1], self.grid[r][c][2]))
+            qp.drawRect(c * self.tile_size_x, r * self.tile_size_y, self.tile_size_x, self.tile_size_y)
+
+
+    def draw_grid(self, event, qp):
+        qp.setPen(QColor(0, 0, 0))
+
+        for r in range(self.grid_rows):
+            for c in range(self.grid_cols):
+                qp.setBrush(QColor(self.grid[r][c][0],self.grid[r][c][1], self.grid[r][c][2]))
+                qp.drawRect(c*self.tile_size_x, r*self.tile_size_y, self.tile_size_x, self.tile_size_y)
+
+
+    def to_grid(self):
+        xrange = Range(-6, 6, self.grid_cols)
+        yrange = Range(0, 2, self.grid_rows)
+        filled = np.zeros_like(self.grid, dtype=np.uint)
+        for r in range(self.grid_rows):
+            for c in range(self.grid_cols):
+                filled[r][c] = [self.grid[r][c][0], self.grid[r][c][1], self.grid[r][c][2]]
+        grid = Grid(xrange, yrange, filled=filled)
+        return grid
+
+
+    def export(self, filename):
+        ## export x, y ranges and grid
+        with open(filename, "w") as f:
+            f.write("x:-6, 6.25, "+ str(self.grid_cols) +"\n")
+            f.write("y:0,2, "+ str(self.grid_rows) + "\n")
+            for r in range(self.grid_rows):
+                for c in range(self.grid_cols):
+                    f.write(str(self.grid[r][c][0]) + "," + str(self.grid[r][c][1]) + "," + str(self.grid[r][c][2]))
+                    if c<self.grid_cols-1:
+                        f.write(",")
+                f.write("\n")
+
+
+class QColorButton(QPushButton):
+
+    def __init__(self, *args, **kwargs):
+        super(QColorButton, self).__init__(*args, **kwargs)
+
+        self.color = "#FFFFFF"
+        self.setMaximumWidth(64)
+        self.pressed.connect(self.onColorPicker)
+
+    def set_color(self, color):
+        if color != self.color:
+            self.color = color
+
+        if self.color:
+            self.setStyleSheet("background-color: %s;" % self.color)
+        else:
+            self.setStyleSheet("")
+
+    def color(self):
+        return self.color
+
+    def color_rgb(self):
+        c = QColor(self.color)
+        return [c.red(), c.green(), c.blue()]
+
+    def onColorPicker(self):
+        dlg = QColorDialog(self)
+        if self.color:
+            dlg.setCurrentColor(QColor(self.color))
+
+        if dlg.exec_():
+            self.set_color(dlg.currentColor().name())
+
+    def mousePressEvent(self, e):
+        if e.button() == Qt.RightButton:
+            self.set_color(None)
+
+        return super(QColorButton, self).mousePressEvent(e)
+
+
+class ControlWidget(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+
+        self.parent = parent
+
+        self.t_funcs = TFunctions()
+        self.cur_t_func = TMatrix.create_identity_map()
+
+        self.color_button = QColorButton()
+
+        self.check_lines = QCheckBox("Draw grid")
+        self.check_lines.setChecked(False)
+        self.check_lines.stateChanged.connect(lambda: self.check_btn_state(self.check_lines))
+
+        self.combo_func = QComboBox()
+        for t_func in self.t_funcs.f:
+            self.combo_func.addItem(t_func.get("name"))
+
+        self.param_sliders = []
+
+        self.combo_func.activated[str].connect(self.onComboActivated)
+
+        self.layout = QFormLayout()
+
+        self.layout.addRow("Color: ", self.color_button)
+        self.layout.addRow("Do:", self.check_lines)
+        self.layout.addRow("Func:", self.combo_func)
+
+        self.setLayout(self.layout)
+
+
+    def onComboActivated(self, name):
+        for slider in self.param_sliders:
+            self.layout.removeRow(slider)
+        self.param_sliders = []
+        args = self.t_funcs.get_f_args_by_name(name)
+        if args:
+            for arg in args:
+                slider = QSlider(Qt.Horizontal)
+                slider.setMinimum(0)
+                slider.setMaximum(100)
+                slider.setValue(50)
+                slider.setTickInterval(1)
+                slider.valueChanged[int].connect(self.slider_val_changed)
+                self.param_sliders.append(slider)
+                self.layout.addRow(arg[0], slider)
+        self.update_output_canvas()
+
+
+    def slider_val_changed(self, value):
+        print("value=", value)
+        self.update_output_canvas()
+
+
+    def update_output_canvas(self):
+        func_name = self.combo_func.currentText()
+        self.cur_t_func = self.compose_func(func_name)
+        self.parent.update_output_canvas()
+
+
+    def compose_func(self, f_name):
+        func = self.t_funcs.get_f_func_by_name(f_name)
+        args = self.t_funcs.get_f_args_by_name(f_name)
+        f_inputs = []
+        if args:
+            for i,arg in enumerate(args):
+                v_min = arg[1]
+                v_max = arg[2]
+                v = v_min + self.param_sliders[i].value()/100 * (v_max - v_min)
+                f_inputs.append(v)
+        return func(f_inputs)
+
+
+    def check_btn_state(self, b):
+        if b.text() == "Draw grid":
+            self.parent.update_output_canvas()
+
+
+
+class OutputWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.label = QLabel(self)
+        self.pixmap = QPixmap()
+        self.label.setPixmap(self.pixmap)
+
+    def update_pixmap(self, np_arr):
+        width, height, ch = np_arr.shape
+        bytes_per_line = ch*width
+        qimage = QImage(np_arr.data, width, height, bytes_per_line, QImage.Format_RGB888)
+        self.label.setPixmap(QPixmap.fromImage(qimage))
+        self.setMinimumSize(qimage.width(), qimage.height())
+
+
+class MainWidget(QWidget):
+    def __init__(self, img_file):
+        super().__init__()
+        self.initUI(img_file)
+
+
+    def initUI(self, img_file):
+        self.control_widget = ControlWidget(self)
+
+        self.grid_widget_src = GridWidget(self, self.control_widget)
+        self.grid_widget_src.from_image(img_file)
+
+        self.output_widget = OutputWidget()
+        self.update_output_canvas()
+
+        self.left_arrow_label = QLabel("  >>  ")
+        font = QFont()
+        font.setPixelSize(25)
+        self.left_arrow_label.setFont(font)
+
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.control_widget)
+        self.layout.addWidget(self.grid_widget_src)
+        self.layout.addWidget(self.left_arrow_label)
+        self.layout.addWidget(self.output_widget)
+
+        self.setLayout(self.layout)
+
+        self.setWindowTitle('CTransformEditor')
+
+        self.show()
+
+
+    def keyPressEvent(self, event):
+        self.grid_widget.keyPressEvent(event)
+
+
+    def update_output_canvas(self):
+        t_grid = self.grid_widget_src.to_grid().transform_by(self.control_widget.cur_t_func)
+        canvas = t_grid.draw_on_canvas(do_fill=True,
+                                       do_outlines=self.control_widget.check_lines.isChecked())
+        self.output_widget.update_pixmap(canvas)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mw = MainWidget("test.png")
+    sys.exit(app.exec_())
+
+
---
+++ CTransformGrid.py
@@ -0,0 +1,373 @@
+import numpy as np
+import typing
+from typing import Iterable
+import sys
+import cv2
+
+
+class Range:
+    def __init__(self, min=0, max=1, len=100):
+        assert(min <= max)
+        assert(len > 0)
+        self.min = min
+        self.max = max
+        self.len = len
+
+    def to_np(self):
+        return np.linspace(self.min, self.max, self.len)
+
+    def size(self):
+        return self.len
+
+
+
+## Cayley map: K(z) = (z-i)/(z+i)
+def _cayley_single(z):
+    return (z-1j)/(z+1j)
+
+
+def _cayley_list(z:Iterable[complex]):
+    return [(zz-1j)/(zz+1j) for zz in z]
+
+
+def cayley(z):
+    if type(z)=="list":
+        return _cayley_list(z)
+    else:
+        return _cayley_single(z)
+
+
+def get_bounds(vs:Iterable[complex]):
+
+    min_x = min_y = sys.maxsize
+    max_x = max_y = -sys.maxsize
+
+    for a in vs:
+        if a.real < min_x:
+            min_x = a.real
+        if a.real > max_x:
+            max_x = a.real
+        if a.imag < min_y:
+            min_y = a.imag
+        if a.imag > max_y:
+            max_y = a.imag
+    return { 'min_x':min_x, "max_x":max_x, "min_y":min_y, "max_y":max_y }
+
+
+def scale_in_range(vs, min_x, max_x, min_y, max_y):
+    b = get_bounds(vs)
+    scale_x = (max_x - min_x) / (b['max_x'] - b['min_x'])
+    scale_y = (max_y - min_y) / (b['max_y'] - b['min_y'])
+
+    sv = []
+
+    for cvv in vs:
+        sv_x = int((cvv.real + abs(b['min_x'])) * scale_x)
+        sv_y = int((cvv.imag + abs(b['min_y'])) * scale_y)
+        sv.append(sv_x + sv_y*1j)
+    return sv
+
+
+class Grid:
+
+    def __init__(self, xrange:Range, yrange:Range, points=None, filled=None):
+        self._xrange    = xrange
+        self._yrange    = yrange
+        if points is None:
+            self._points = np.array([[i + k * 1j for i in xrange.to_np()] for k in yrange.to_np()])
+        else:
+            self._points = points
+        if filled is None:
+            ## add color channel and decrease xy dim by one
+            self._filled = np.ones(np.subtract(self._points.shape + (3,), (1,1,0))) * 128
+        else:
+            self._filled = filled
+
+
+    @staticmethod
+    def from_file(filename):
+        import re
+        with open(filename, "r") as f:
+            xrange = None
+            yrange = None
+            lines = f.readlines()
+
+            hit_x = re.search(r'\s*x:\s*(\S+)\s*,\s*(\S+)\s*,\s*(\S+)', lines[0], re.M | re.I)
+            if hit_x:
+                xrange = Range(float(hit_x.group(1)), float(hit_x.group(2)), int(hit_x.group(3)))
+                print("min=", float(hit_x.group(1)))
+                print("max=", float(hit_x.group(2)))
+                print("len=", int(hit_x.group(3)))
+
+            hit_y = re.search(r'\s*y:\s*(\S+)\s*,\s*(\S+)\s*,\s*(\S+)', lines[1], re.M | re.I)
+            if hit_y:
+                yrange = Range(float(hit_y.group(1)), float(hit_y.group(2)), int(hit_y.group(3)))
+                print("min=", float(hit_y.group(1)))
+                print("max=", float(hit_y.group(2)))
+                print("len=", int(hit_y.group(3)))
+
+            filled = np.zeros((yrange.len, xrange.len, 3))
+            for i in range(2, len(lines)):
+                filled[i-2,:,:] = np.array([float(id) for id in lines[i].split(",")]).reshape(xrange.len, 3)
+
+            if xrange is None or yrange is None:
+                sys.exit("invalid file")
+
+            return Grid(xrange, yrange, filled=filled)
+
+
+    def get_points(self):
+        return self._points.ravel()
+
+
+    def get_filled(self):
+        return self._filled
+
+
+    def get_range(self, dir):
+        if dir=="x":
+            return self._xrange
+        else:
+            return self._yrange
+
+
+    def transform_by(self, M):
+        cp = np.ones(self._points.shape) * (0+0j)
+        for i in range(self._yrange.len):
+            for j in range(self._xrange.len):
+                cp[i][j] = M(self._points[i][j])
+            #print("=>", p, "->", cp[i])
+        b = get_bounds(cp.ravel())
+        xrange = Range(b['min_x'], b['max_x'], self._xrange.len)
+        yrange = Range(b['min_y'], b['max_y'], self._yrange.len)
+        return Grid(xrange, yrange, cp, self._filled)
+
+
+    def transform_rescale(self, min_x, max_x, min_y, max_y):
+        cur_xmin = self._xrange.min
+        cur_xmax = self._xrange.max
+        cur_ymin = self._yrange.min
+        cur_ymax = self._yrange.max
+        scale_x = (max_x - min_x) / (cur_xmax - cur_xmin)
+        scale_y = (max_y - min_y) / (cur_ymax - cur_ymin)
+
+        points = np.ones(self._points.shape) * (0+0j)
+        for i in range(self._yrange.len):
+            for j in range(self._xrange.len):
+                p = self._points[i][j]
+                real = int((p.real-cur_xmin) * scale_x + min_x)
+                imag = int((p.imag-cur_ymin) * scale_y + min_y)
+                points[i][j] = real + imag*1j
+        xrange = Range(min_x, max_x, self._xrange.len)
+        yrange = Range(min_y, max_y, self._yrange.len)
+
+        return Grid(xrange, yrange, points, self._filled)
+
+
+    def draw_on_canvas(self, canvas_w=500, canvas_h=500, do_fill=True, do_outlines=False):
+        my_grid = self.transform_rescale(0, canvas_w - 1, 0, canvas_h - 1)
+        canvas = np.ones((canvas_h, canvas_w, 3), dtype=np.uint8) * 40
+
+        white = (255,) * 3
+        black = (0,) * 3
+
+        if do_fill:
+            for cell in Grid.CellIter(my_grid):
+                pts = np.array([[c.real, c.imag] for c in cell[0]])
+                col = cell[1][::-1]
+                cv2.fillPoly(canvas, np.int32([pts]), color=(int(col[0]), int(col[1]), int(col[2])))
+
+        if do_outlines:
+            ## draw cayley transformed points
+            for point in my_grid.get_points():
+                cv2.circle(canvas, (int(point.real), int(point.imag)), 2, white, -1)
+
+            ## draw gridlines
+            for row in Grid.RowIter(my_grid):
+                for i in range(1, len(row)):
+                    p1x = int(row[i - 1].real)
+                    p1y = int(row[i - 1].imag)
+                    p2x = int(row[i].real)
+                    p2y = int(row[i].imag)
+                    cv2.line(canvas, (p1x, p1y), (p2x, p2y), black, 1)
+
+            for col in Grid.ColIter(my_grid):
+                for i in range(1, len(col)):
+                    p1x = int(col[i - 1].real)
+                    p1y = int(col[i - 1].imag)
+                    p2x = int(col[i].real)
+                    p2y = int(col[i].imag)
+                    cv2.line(canvas, (p1x, p1y), (p2x, p2y), black, 1)
+        canvas = canvas[:,:,(2,1,0)].copy()
+        return canvas
+
+
+    class RowIter:
+        def __init__(self, parent):
+            self._cur_row = 0
+            self._parent = parent
+            self._nrows = parent._yrange.size()
+            self._ncols = parent._xrange.size()
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            if self._cur_row >= self._nrows:
+                raise StopIteration
+            else:
+                self._cur_row += 1
+                return self._parent._points[self._cur_row-1]
+
+
+    class ColIter:
+        def __init__(self, parent):
+            self._cur_col = 0
+            self._parent = parent
+            self._nrows = parent._yrange.size()
+            self._ncols = parent._xrange.size()
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            if self._cur_col >= self._ncols:
+                raise StopIteration
+            else:
+                idx = [[i for i in range(self._nrows)], [self._cur_col for i in range(self._nrows)]]
+                self._cur_col += 1
+                return self._parent._points[idx]
+
+
+    class CellIter:
+        def __init__(self, parent):
+            self._parent = parent
+            self._nrows = parent._yrange.size()
+            self._ncols = parent._xrange.size()
+            self._cur_row = 0
+            self._cur_col = -1
+            self._niter = 0
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            self._cur_col += 1
+
+            if self._cur_col == self._ncols-1:
+                self._cur_col = 0
+                self._cur_row += 1
+
+            self._niter += 1
+
+            if self._niter >= (self._nrows-1)*(self._ncols-1):
+                raise StopIteration
+
+            idx = [[self._cur_row]*2 +  [self._cur_row+1]*2,  # row idx
+                   [self._cur_col, self._cur_col+1, self._cur_col+1, self._cur_col]]     # col idx
+            return (self._parent._points[idx], self._parent._filled[self._cur_row, self._cur_col, :])
+
+
+class TMatrix:
+    def __init__(self, a, b, c, d):
+        self._a = a
+        self._b = b
+        self._c = c
+        self._d = d
+
+    def __call__(self, z:complex) -> complex:
+        return (self._a*z + self._b)/(self._c*z + self._d)
+
+    @staticmethod
+    def create_identity_map(p=[]):
+        return TMatrix(1 + 0j, 0 + 0j, 0 + 0j, 1 + 0j)
+
+    @staticmethod
+    def create_hyperbolic_map(p):
+        u = p[0]
+        import cmath
+        assert(u>1)
+        return TMatrix(u+0j, cmath.sqrt(u*u-1)+0j, cmath.sqrt(u*u-1)+0j, u+0j)
+
+    @staticmethod
+    def create_rotation_map(p):
+        theta = p[0]
+        import cmath
+        return TMatrix(cmath.exp(theta*1j), 0, 0, cmath.exp(-theta*1j))
+
+    @staticmethod
+    def create_cayley_map(p=[]):
+        return TMatrix(1, -1j, 1, 1j)
+
+    @staticmethod
+    def create_moebius_map(p):
+        a = complex(p[0], p[1])
+        b = complex(p[2], p[3])
+        c = complex(p[4], p[5])
+        d = complex(p[6], p[7])
+        return TMatrix(a, b, c, d)
+
+
+class CLinFunc:
+
+    def __init__(self, func=lambda z:z, a=complex(1,0), b=complex(0,0)):
+        self._a = a
+        self._b = b
+        self.func = func
+
+    def __call__(self, z:complex) -> complex:
+        return self.func(self._a*z) + self._b
+
+
+
+class TFunctions:
+
+    def __init__(self):
+
+        self.f = [
+            {
+                "name": "identity_map",
+                "args": None,
+                "func": TMatrix.create_identity_map
+            },
+            {
+                "name" : "caylay_map",
+                "args" : None,
+                "func" : TMatrix.create_cayley_map
+            },
+            {
+                "name" : "rotation_map",
+                "args" : [["angle", 0, 360, 0, 1]],
+                "func" : TMatrix.create_rotation_map
+            },
+            {
+                "name" : "hyperbolic_map",
+                "args" : [["p", 1, 2, 1, 0.001]],
+                "func" : TMatrix.create_hyperbolic_map
+            },
+            {
+                "name" : "moebius_map",
+                "args" : [["a_r", -2, 4, 1, 0.1],
+                          ["a_i", -2, 2, 0, 0.1],
+                          ["b_r", -2, 2, 0, 0.1],
+                          ["b_i", -2, 2, 0, 0.1],
+                          ["c_r", -2, 2, 0, 0.1],
+                          ["c_i", -2, 2, 0, 0.1],
+                          ["d_r", -2, 4, 0, 0.1],
+                          ["d_i", -2, 2, 0, 0.1],
+                          ],
+                "func" : TMatrix.create_moebius_map
+            }
+        ]
+
+    def get_f_args_by_name(self, name):
+        for f in self.f:
+            if f["name"] == name:
+                return f["args"]
+        return None
+
+    def get_f_func_by_name(self, name):
+        for f in self.f:
+            if f["name"] == name:
+                return f["func"]
+        return None
